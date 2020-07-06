9f94064c0a9277f358dcc88967cf91b962d461e7
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-07-06 13:07:50 +1000

    Edit comments
    Removed the noise comments I'd written originally as a top level guide
    to what needed to be done, and included a comment trying to context on
    why we're decoding the value of the query param before testing it.

--- maskQueryParams.js
+++ maskQueryParams.js
@@ -8,17 +8,14 @@ function maskQueryParams(request) {
         
         // TODO: Check this chonky boi meets what we need for email validation
         var emailRegexp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-        // iterate queryParams
+
         var filteredQueryParams = queryParams.map(queryParam => {
-            // URI decode value
             var kv = queryParam.split('=');
             var key = kv[0];
             var value = kv [1];
+            // decoding the value in the event it's percent encoded
             var decodedValue = decodeURIComponent(value);
-            // check value
-            // if value matches regex string for email addresses
             if (emailRegexp.test(decodedValue)) {
-                // replace/construct with [EMAIL FILTERED]
                 value = '[EMAIL FILTERED]'
             }
 
